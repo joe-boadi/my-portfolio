932ca2efbde1019901ed1c03650fa66d139e14c8
joe-boadi <joeboadi1@gmail.com>
2024-03-27 16:30:03 +0000

    Update content with TODOs

---
+++ app/Components/Sidebar.tsx
@@ -0,0 +1,24 @@
+
+{/*TODO*/}
+{/**
+    Function to handle hire me button 
+    const onHireMe = () => {
+        
+    }
+    
+*/}
+
+export default function Sidebar(){
+    return(
+        <div className="bg-slate-900">
+            {/*TODO*/}
+            {/* Profile picture/Hero Image with  */}
+
+            {/*Experience, Location, status & "Hire me" button*/}
+
+            {/* Social links*/}
+        
+        </div>
+    )
+ 
+}
---
+++ app/Components/Topnav.tsx
@@ -0,0 +1,18 @@
+export default function Topnav(){
+    return (
+
+        <nav className="bg-slate-800 p-4">
+            {/*TODO*/}
+            {/*Portfolio Logo */}
+
+            <div>
+                {/*TODO*/}
+                {/* Page Links goes here*/}
+                <a href="/" target="" rel="noopener noreferrer">Home</a>
+                <a href="/" target="" rel="noopener noreferrer">Profile</a>
+                <a href="/" target="" rel="noopener noreferrer">Project</a>
+                <a href="/" target="" rel="noopener noreferrer">Contact</a>
+            </div>
+        </nav>
+    )
+}
