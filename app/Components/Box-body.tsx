f7af27d05bcb59c65663bdcabd18dab9f40be12f
joe-boadi <joeboadi1@gmail.com>
2024-04-01 22:23:22 +0000

    Add empty div container

---
+++ app/Components/Box-body.tsx
@@ -0,0 +1,13 @@
+export default function MainBody(){
+    return(
+        <>
+        {/* Display grid containers for project */}
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        </>
+    )
+}
