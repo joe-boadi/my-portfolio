8af6120794813f81f9a54bc1eabb21751a516f62
joe-boadi <joeboadi1@gmail.com>
2024-05-14 01:54:00 +0000

    chore: Style icon to vary in size on different screen res

--- app/utils/StackDisplay.tsx
+++ app/utils/StackDisplay.tsx
@@ -8,76 +8,78 @@ import { RiNextjsFill } from "react-icons/ri";
 import { VscVscodeInsiders } from "react-icons/vsc";
 
 type StackLink = {
-    icon: JSX.Element;
+    icon: JSX.Element & { className?: "w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" };
     name: string;
 }
 
 const stackIcons: StackLink[] = [
     {
-        icon: <DiJavascript/>,
+        icon: <DiJavascript className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Javascript"
     },
     {
-        icon: <IoLogoReact />,
+        icon: <IoLogoReact className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "React"
     },
     {
-        icon: <RiNextjsFill />,
+        icon: <RiNextjsFill className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Next.js"
     },
     {
-        icon: <BiLogoTypescript />,
+        icon: <BiLogoTypescript className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "TypeScript"  
     },
     {
-        icon: <FaPython />,
+        icon: <FaPython className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Python"
     },
     {
-        icon: <FaTrello />,
+        icon: <FaTrello className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Trello"
     },
     {
-        icon: <FaNodeJs />,
+        icon: <FaNodeJs className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Node.js"
     },
     {
-        icon: <FaHtml5 />,
+        icon: <FaHtml5 className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "HTML5"
     },
     {
-        icon: <FaCss3 />,
+        icon: <FaCss3 className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "CSS3"
     },
     {
-        icon: <VscVscodeInsiders />,
+        icon: <VscVscodeInsiders className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "VS Code"
     },
     {
-        icon: <FaGolang />,
+        icon: <FaGolang className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Golang"
     },
     {
-        icon: <FaDiscord />,
+        icon: <FaDiscord className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Discord"
     },
     {
-        icon: <FaSlack />,
+        icon: <FaSlack className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Slack"
     },
     {
-        icon: <FaGithub />,
+        icon: <FaGithub className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16" />,
         name: "Github"
     },
 ];
 
 const StackIcons = () => {
     return (
-        <div className="grid md:grid-cols-2 m-2 p-1 justify-center text-sm">
+        <div className="grid lg:grid-cols-5 md:grid-cols-2 m-0 p-1 justify-center">
             {stackIcons.map((icon, index) => (
-                <svg key={index} className="m-2 p-1 justify-center items-center text-sm">
-                    {icon.icon}
-                </svg>
+                <div key={index} className="m-2 p-1 justify-center items-center h-full w-full">
+                    <div className="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16">
+                        {icon.icon}
+                    </div>
+                </div>
             ))}
         </div>
     )
