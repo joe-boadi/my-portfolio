392a416477955b79982bea88286b89903c724587
joe-boadi <joeboadi1@gmail.com>
2024-05-14 01:02:38 +0000

    Style page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Body from "./Components/Body";
 
 export default function Home() {
   return (
-    <main >
+    <main className="h-screen font-mono">
       <NavLinks />
       <Sidebar />
       <Body />
