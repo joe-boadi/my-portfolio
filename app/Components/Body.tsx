ffcf2254294881f6df2b415ffedf77c65d2f3f88
joe-boadi <joeboadi1@gmail.com>
2024-05-14 01:53:04 +0000

    Modify Body file

--- app/Components/Body.tsx
+++ app/Components/Body.tsx
@@ -1,3 +1,4 @@
+import { FaTrello } from "react-icons/fa";
 import StackIcons from "../utils/StackDisplay";
 
 export default function MainBody() {
@@ -27,9 +28,9 @@ export default function MainBody() {
             </div>
 
             <div className="text-3xl p-4 m-2"> <h2>Tools I use</h2> </div>
-            <div className="outline rounded-lg p-3 m-2">
+            <div className="outline rounded-lg p-2 m-1">
                 <div className="">
-                    <div className="flex grid-cols-4 justify-center items-center gap-4">
+                    <div className="flex grid-cols-4 justify-center items-center gap-4 w-full h-full">
                         <StackIcons />
                     </div>
                 </div>
