ea927b5230295b476b4f629bf91745a09400331a
joe-boadi <joeboadi1@gmail.com>
2024-05-15 23:45:10 +0000

    Modify and update file

---
+++ app/Components/Contact.tsx
@@ -0,0 +1,21 @@
+import Styles from '@/app/Styles/Contact.module.css'
+
+export default function Contact () {
+    return (
+        <section className={`${Styles.contactSection} text-center font-mono items-center outline`}>
+            <h2 className={`${Styles.h2} text-base-content`}>Contact</h2>
+            <form action={'#'} method='post' className={Styles.form}>
+            {/* Form fields go here */}
+            <div className={`${Styles.inputGroup} text-base-content`}>
+                <input className={`${Styles.input} bg-base-200`} type="text" id='name' name='name' placeholder='Name' required/>
+                <span className={`${Styles.span} bg-base-200`}></span>
+                <input className={`${Styles.input} bg-base-200`} type="email" name="email" id="email" placeholder='Email' required />
+            </div>
+            <textarea className={`${Styles.textarea} bg-base-200`} name="message" id='message' placeholder='Message required'></textarea>
+            <button className={`${Styles.button} btn btn-outline rounded-lg`} type="submit">Submit</button>
+            </form>
+        </section>
+
+    )
+}
+
