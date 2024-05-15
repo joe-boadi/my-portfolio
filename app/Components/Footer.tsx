ea2faafee44be90cf60c8d0cb4b6eb633f88d787
joe-boadi <joeboadi1@gmail.com>
2024-05-15 22:49:49 +0000

    feat: Add footer

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer () {
+    return(
+        <footer className="items-center p-4 m-3 justify-center bottom-0 text-center font-mono">
+            <p>Copyright &copy; 2024 Made by <a href="https://github.com/joe-boadi">joe-boadi</a>  |  All Rights Reserved</p>
+        </footer>
+    )
+}
