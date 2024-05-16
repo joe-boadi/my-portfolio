74e7a74467c76264ef513a6ce070b928df78aa0e
joe-boadi <joeboadi1@gmail.com>
2024-05-16 01:17:30 +0000

    Update page with <StackIcons />

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Sidebar from "./Components/Sidebar";
 import NavLinks from "./Components/NavLinks";
-import Body from "./Components/Body";
+import Body from "./Components/Sections";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
+import StackIcons from "./utils/StackDisplay";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <NavLinks />
         <Sidebar />
         <Body />
+        <StackIcons />
         <Contact />
         <Footer />
     </body>
