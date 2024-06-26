b808754640b91747694272279e2062da43aa792e
joe-boadi <joeboadi1@gmail.com>
2024-06-26 12:41:01 +0000

    Re-edit metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "joe-boadi's Portfolio",
-  openGraph: {
-    title: "joe-boadi's Portfolio",
-    description: "A portfolio website for Kwadjo Boadi",
-  },
-  icons: "./app/profile.ico",
+  description: "A portfolio website for Kwadjo Boadi",
 };
 
 export default function RootLayout({
@@ -20,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en"> 
+      <body className={`${inter.className} items-center`}>{children}</body>
     </html>
   );
 }
