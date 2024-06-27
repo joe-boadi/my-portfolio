b90425a4ce74994c83ea6c0bb136d94d451d990d
joe-boadi <joeboadi1@gmail.com>
2024-06-27 21:17:52 +0000

    TSX list components should not use array indexes as key

--- app/Components/NavLinks.tsx
+++ app/Components/NavLinks.tsx
@@ -32,7 +32,7 @@ const NavLinks = () => {
           </header>
           <div className="hidden md:flex space-x-6 md: ml-16 pl-16 font-bold right-4 text-sm">
             {navLinks.map((link, index) => (
-              <Link key={index} href={link.href}>
+              <Link key={link.href} href={link.href}>
                 <div className="hover:text-green-500 hover:underline m-2">{link.label}</div>
               </Link>
             ))}
@@ -47,7 +47,7 @@ const NavLinks = () => {
               <div className="fixed right-0 z-20 w-full p-12 flex flex-col justify-center items-center lg:hidden md:hidden bg-opacity-75 backdrop-blur-lg">
               <ul>
                 {navLinks.map((link, index) => (
-                  <Link key={index} href={link.href}>
+                  <Link key={link.href} href={link.href}>
                       <li className='hover:text-green-500 hover:underline active:text-green-400 active:underline'>{link.label}</li>
                   </Link>
                 ))}
