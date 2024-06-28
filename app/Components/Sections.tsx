5f530d1fffcfa48c233b3dcf9be0945a69f0747e
joe-boadi <joeboadi1@gmail.com>
2024-06-28 00:18:32 +0000

    Position the div right

--- app/Components/Sections.tsx
+++ app/Components/Sections.tsx
@@ -8,7 +8,7 @@ const Section = () =>{
     
     return(
             <section id="profile" className="w-4/5 p-9 m-3">
-                <div className="container flex md:flex-col ml-0 pl-0 left-0 justify-center mt-16 m-1 lg:mx-3 font-mono">
+                <div className="container flex md:flex-col ml-0 pl-0 left-0 justify-center mt-16 lg:mx-3 font-mono">
                     <div className="w-full min-h-screen">
                         <Introduction />
                         <StackIcons />
