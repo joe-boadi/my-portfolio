c5cd8e4a4205b861956f9fdfd9b02ca900d49238
joe-boadi <joeboadi1@gmail.com>
2024-06-28 00:02:58 +0000

    Add margin and padding to section

--- app/Components/Sections.tsx
+++ app/Components/Sections.tsx
@@ -7,7 +7,7 @@ import Introduction from "./Introduction";
 const Section = () =>{
     
     return(
-            <section id="profile w-4/5">
+            <section id="profile" className="w-4/5 p-9 m-3">
                 <div className="container flex md:flex-col ml-0 pl-0 left-0 justify-center mt-16 m-1 lg:mx-3 font-mono">
                     <div className="w-full min-h-screen">
                         <Introduction />
