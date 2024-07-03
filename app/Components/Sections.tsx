52e2c28fcc0cfffa7e0aa466d9f062abd9de5d80
joe-boadi <joeboadi1@gmail.com>
2024-07-03 11:45:06 +0000

    Update/Modify layout of section

--- app/Components/Sections.tsx
+++ app/Components/Sections.tsx
@@ -7,9 +7,9 @@ import Introduction from "./Introduction";
 const Section = () =>{
     
     return(
-            <section id="profile" className="w-4/5 p-9 m-3">
-                <div className="container flex md:flex-col ml-0 pl-0 left-0 justify-center mt-16 lg:mx-3 font-mono">
-                    <div className="w-full min-h-screen">
+            <section id="profile" className="w-4/5 p-9 m-3 text-sm">
+                <div className="container flex md:flex-col justify-center mt-16 font-mono text-sm">
+                    <div className="min-h-screen">
                         <Introduction />
                         <StackIcons />
                         <ProjectDisplay />
