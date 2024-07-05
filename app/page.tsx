95b2bd6a4c23bec9cf960b13a87be762fbeabd59
joe-boadi <joeboadi1@gmail.com>
2024-07-05 12:54:50 +0000

    Re-structure components with overloaded component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import Sidebar from "./Components/Sidebar";
-import NavLinks from "./Components/NavLinks";
 import Sections from "./Components/Sections";
-import Footer from "./Components/Footer";
 
 const Home = () => {
   return (
-    <main className="container m-auto p-4">
-      <NavLinks />
-      <div className="container flex grid-cols-2 p-1 gap-0">
-          <Sidebar />
-          <Sections />
-      </div>
-      <Footer />
-    </main>
+      <>
+        <Sections />
+      </>
   );
 }
 
-export default Home
+export default Home;
