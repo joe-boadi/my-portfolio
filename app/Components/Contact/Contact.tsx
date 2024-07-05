a9554941db6766b6bbae29c926d3263e5bad81a4
joe-boadi <joeboadi1@gmail.com>
2024-07-05 12:52:41 +0000

    Style and modify contact form

--- app/Components/Contact/Contact.tsx
+++ app/Components/Contact/Contact.tsx
@@ -36,19 +36,20 @@ const Contact = () => {
     }
 
     return (
-        <section id='contact' className={`${Styles.contactSection} text-sm text-center mt-12 font-mono items-center border rounded-lg hover:bg-base-300`}>
+        <section id='contact' className={`${Styles.contactSection} text-sm text-center mt-12 font-mono items-center border rounded-lg hover:bg-base-300 bottom-6`}>
             <h2 className={`${Styles.h2} mb-6 text-base-content`}>Contact</h2>
             <form action={'#'} method='post' onSubmit={handleSubmit} className={`${Styles.form}`}>
                 {/* Form fields go here */}
                 <div className={`${Styles.inputGroup} text-base-content`}>
+
                     <input className={`${Styles.input} bg-base-200 mb-4`} 
                         type="text" 
                         id='name' 
                         name='name' 
                         placeholder='Name' required
                         onChange={handleChange}
-                        />
-                    <span className={`${Styles.span} bg-base-200`}></span>
+                    />
+
                     <input className={`${Styles.input} bg-base-200`} 
                         type="email" 
                         name="email" 
@@ -57,13 +58,19 @@ const Contact = () => {
                         onChange={handleChange}
                         />
                 </div>
+
                 <textarea className={`${Styles.textarea} bg-base-200`} 
                     name="message" 
                     id='message' 
                     placeholder='Message required'
                     onChange={handleChange}
-                    ></textarea>
-                <button className={`${Styles.button} btn btn-outline hover:bg-green-400`} type="submit">Submit</button>
+
+                ></textarea>
+
+                <button 
+                    className={`${Styles.button} btn btn-outline hover:bg-green-400`} 
+                    type="submit"
+                >Submit</button>
             </form>
         </section>
 
