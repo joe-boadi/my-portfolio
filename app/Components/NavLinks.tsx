9f2fda0a02c62d3973dcaa8537be8dbefa89b173
joe-boadi <joeboadi1@gmail.com>
2024-07-25 14:11:24 +0000

    fix(nav): correct active link highlighting and syntax errors

--- app/Components/NavLinks.tsx
+++ app/Components/NavLinks.tsx
@@ -1,5 +1,6 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Logo from "@/app/assets/logo.png"
 import Image from 'next/image';
@@ -17,10 +18,16 @@ const navLinks: NavLink[] = [
 ];
 
 const NavLinks = () => {
-  const [ mobileDrawerOpen, setMobileDrawerOpen ] = useState(false);
 
-  const toggleNavbar = () => {
-    setMobileDrawerOpen(!mobileDrawerOpen)
+  const [ isMounted, setIsMounted ] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [router]);
+
+  if(!isMounted) {  
+    return null; // Render null until component is fully mounted.
   }
 
   return (
@@ -38,11 +45,16 @@ const NavLinks = () => {
             <h1 className="font-extrabold text-2xl hover:text-green-400"><Link href="#">joe-boadi</Link></h1>
           </header>
           <div className="hidden md:flex space-x-6 md: ml-16 pl-16 font-bold text-sm">
-            {navLinks.map((link) => (
-              <Link key={link.href} href={link.href}>
-                <div className="hover:text-green-500 hover:underline m-2">{link.label}</div>
-              </Link>
-            ))}
+             {navLinks.map((link) => {
+              // const isActive = router.pathname === link.href ${isActive ? 'text-green-500' : ''};
+              return (
+                <Link key={link.href} href={link.href}>
+                  <div className={`hover:text-green-500 hover:underline m-2`}>
+                    {link.label}
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </div>
